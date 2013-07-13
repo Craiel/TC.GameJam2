fe1ad982ffc355722706e0e5d7d7adc67b7145f2
Brent Ellison <brentcarrellison@gmail.com>
2013-07-12 23:04:25 -0400

    First scene plus some random stuff.

---
+++ Assets/scripts/BallScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	public GameObject ballObject;
+	
+	// Use this for initialization
+	void Start () {
+		
+		//rigidbody.AddForce (100, 0, 100);
+		
+		var newball = Instantiate (ballObject,transform.position,transform.rotation);
+	
+	}
+	
+	void FixedUpdate () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
