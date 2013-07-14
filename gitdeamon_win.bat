f72ad5cdeae627ebccbe725c83a44b16d9e4fd45
Patrick <>
2013-07-13 17:32:23 -0700

    - added windows batch file to run the git deamon

---
+++ gitdeamon_win.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Git\bin\git.exe" config pack.windowMemory 10m
+"C:\Program Files (x86)\Git\bin\git.exe" config pack.packSizeLimit 20m
+"C:\Program Files (x86)\Git\bin\git.exe" daemon --reuseaddr --base-path=. --export-all --verbose
